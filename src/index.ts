596f749ac76f535d9932bba0de7b248c395b5fbc
TetroGem <benjamin11220364@gmail.com>
2023-01-15 19:49:31 -0600

    Added hex and bin literal types

--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,18 @@ import { Bytes } from "./Bytes";
 import { convertBytes } from "./convert-bytes";
 import { interpretOn } from "./interpreter";
 
-const mem = new Bytes(128);
+const mem = new Bytes(9);
 
 // 0 1 2 3 4 5 6 7
 // a b _a_ _b_ _s_
 
 const code = `
+add 0 8u1
 
-inp 6 0
-
-add 0 16u1
-add 2 16u1
-
-out 0 2
-out 2 2
-
-jni 8$6 16
-add 4 16$0
-add 4 16$2
-
-out 4 2
-
-clr 0 2
-add 0 16$2
-clr 2 2
-add 2 16$4
-clr 4 2
-
-sub 6 8u1
-jmp 0 5
-
+add 5 8$0
+and 5 8u2
+clr 0 1
+add 0 8$5
 
 end 0 0
 
